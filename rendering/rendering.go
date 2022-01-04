f5391b00b5841d5fc1660b8ddb7bbc5786aaf3cf
Daniel Schmidt <info@daniel-schmidt.me>
2022-01-04 01:29:35 +0100

    Fixed wrong width in printing.

--- rendering/rendering.go
+++ rendering/rendering.go
@@ -33,7 +33,7 @@ func RenderHTML(html string, width int) (image.Image, error) {
 	page := browser.MustPage("data:text/html," + url.PathEscape(html))
 	page.MustWaitLoad()
 
-	imageData, err := page.MustSetViewport(500, 100000, 1.0, false).MustElement("body").Screenshot(proto.PageCaptureScreenshotFormatPng, 100)
+	imageData, err := page.MustSetViewport(width, 100000, 1.0, false).MustElement("body").Screenshot(proto.PageCaptureScreenshotFormatPng, 100)
 	if err != nil {
 		return nil, err
 	}

1639e35ddb872f8c1a58198c1ea63fcfb1b2c849
Daniel Schmidt <info@daniel-schmidt.me>
2022-11-29 02:28:42 +0100

    Added dice roller to generators.

--- frontend/src/js/core/generator.js
+++ frontend/src/js/core/generator.js
@@ -1,14 +1,25 @@
 import { render as renderTemplate } from './templating';
 
-// Add a seedable rng to the template
-// https://github.com/davidbau/seedrandom
+// Add a seedable rng + dice roller to the template
+// PRNG: https://github.com/davidbau/seedrandom
+// Dice Roller: https://dice-roller.github.io/documentation/
 //
 // TODO: include it locally
 const rngScript = (seed) => {
 	return `
 		<script src="//cdnjs.cloudflare.com/ajax/libs/seedrandom/3.0.5/seedrandom.min.js"></script>
+		<script src="https://unpkg.com/mathjs@9.3.2/lib/browser/math.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/random-js@2.1.0/dist/random-js.umd.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@dice-roller/rpg-dice-roller@5.2.1/lib/umd/bundle.min.js"></script>
 		<script>
 			window.random = new Math.seedrandom('${seed}');
+			window.dice = new rpgDiceRoller.DiceRoller();
+			
+			window.dice.engine = {
+			  next () {
+				return Math.abs(window.random.int32());
+			  },
+			};
 		</script>
 `;
 };

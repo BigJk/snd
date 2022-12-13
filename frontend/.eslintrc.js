97361c5f8f6ea45635caa6fcf56117404d6f17a9
Daniel Schmidt <info@daniel-schmidt.me>
2022-12-13 09:18:08 +0100

    Added more ESLint rules.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,6 +3,14 @@ module.exports = {
 		browser: true,
 		es6: true
 	},
+	settings: {
+		react: {
+			createClass: "m",
+			pragma: "m",
+			fragment: "Fragment",
+			version: "detect",
+		},
+	},
 	extends: ['eslint:recommended'],
 	overrides: [],
 	parserOptions: {
@@ -13,10 +21,16 @@ module.exports = {
 			modules: true
 		}
 	},
-	plugins: ['local'],
+	plugins: ['local', 'react'],
 	rules: {
+		"react/self-closing-comp": ["error", {
+			"component": true,
+			"html": true
+		}],
+		'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'ignore', propElementValues: 'always' }],
 		'no-unused-vars': ['error', { 'argsIgnorePattern': 'vnode' }],
 		'arrow-body-style': ['error', 'as-needed'],
+		'jsx-quotes': ['error', 'prefer-single'],
 		'local/jsx-uses-my-pragma': 'error',
 		'local/jsx-uses-vars': 'error',
 		'no-mixed-spaces-and-tabs': 'off'

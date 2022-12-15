7ac05e1c3c459aec296273f575b55463a71b6687
Daniel Schmidt <info@daniel-schmidt.me>
2022-12-15 21:16:55 +0100

    Added content disclaimer to Workshop page.

--- frontend/src/js/ui/views/workshop/index.jsx
+++ frontend/src/js/ui/views/workshop/index.jsx
@@ -15,6 +15,18 @@ export default function () {
 							<i className='ion ion-md-refresh' onclick={() => store.pub('reload_public_packages')} />
 						</div>
 					</Header>
+					<div className='ph3 pb3'>
+						<div className='pr3'>
+							<div className='toast toast-primary w-50 lh-copy'>
+								<h6>
+									<i className='ion ion-md-warning mr1' /> Attention
+								</h6>
+								The Workshop features community created content. The Sales & Dungeons Team is not responsible for the content contained in the
+								packages. Importing content from here is done on your own risk. If you do find anything inappropriate in one of the packages please
+								inform the Staff on the official Discord.
+							</div>
+						</div>
+					</div>
 					<div className='ph3 flex flex-wrap'>
 						{store.data.publicPackages.map((p, i) => (
 							<PreviewBox

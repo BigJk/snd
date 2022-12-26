64baf20c650d16e1c850836cdd6e89043f60c397
Daniel Schmidt <info@daniel-schmidt.me>
2022-12-26 21:21:26 +0100

    Fix problem with select if oninput callback changed dynamically.

--- frontend/src/js/ui/components/select.jsx
+++ frontend/src/js/ui/components/select.jsx
@@ -28,19 +28,39 @@ export default () => {
 		</select>
 	);
 
-	let dropdown = null;
+	let state = {
+		dropdown: null,
+		oninput: null,
+		open: false
+	}
+
+	// we need to update the oninput value on create and update of component,
+	// otherwise we miss updates to this callback.
+	let setOnInput = (vnode) => {
+		state.oninput = (value) => {
+			vnode.attrs.oninput({ target: { value: value } });
+			m.redraw();
+		};
+	}
 
 	return {
 		oncreate(vnode) {
-			dropdown = easydropdown(vnode.dom.classList.contains('form-select') ? vnode.dom : vnode.dom.querySelector('.form-select'), {
+			setOnInput(vnode);
+
+			state.dropdown = easydropdown(vnode.dom.classList.contains('form-select') ? vnode.dom : vnode.dom.querySelector('.form-select'), {
 				behavior: {
 					maxVisibleItems: 7,
+					liveUpdates: true,
 				},
 				callbacks: {
 					onSelect: (value) => {
-						vnode.attrs.oninput({ target: { value: value } });
-						m.redraw();
+						// instead of passing vnode.attrs.oninput to onSelect we need to redirect
+						// it to the stored oninput, otherwise this onSelect would be fixed to the
+						// callback that was set in oncreate.
+						state.oninput(value);
 					},
+					onOpen: () => (state.open = true),
+					onClose: () => (state.open = false)
 				},
 				classNames: {
 					root: `edd-root ${vnode.attrs.labelCol ? 'col-' + (12 - vnode.attrs.labelCol) : ''}`,
@@ -53,10 +73,10 @@ export default () => {
 			});
 		},
 		onupdate(vnode) {
-			dropdown.refresh();
+			setOnInput(vnode);
 		},
 		onremove(vnode) {
-			dropdown.destroy();
+			state.dropdown.destroy();
 		},
 		view(vnode) {
 			return <FormGroup label={vnode.attrs.label} labelCol={vnode.attrs.labelCol} elem={getSelect(vnode)} />;

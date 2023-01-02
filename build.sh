002929c2a99fe6291620faa1002e47b3a3e834b0
Be Roberts <dissolve@hey.com>
2023-01-02 12:27:57 -0500

    custom build options

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ BUILD_TIME=$(date -Iseconds)
 GO_VERSION=$(go version)
 GIT_COMMIT=$(git rev-list -1 HEAD)
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TARGET_DIR=${SND_RELEASE_DIR:='build/release/'}
+APP_NAME=${SND_APP_NAME:='Sales & Dungeons'}
 
 echo "Build Time : ${BUILD_TIME}"
 echo "Branch     : ${GIT_BRANCH}"
@@ -15,8 +17,10 @@ echo "Build Arch : ${GOARCH:=$(go env GOARCH)}"
 echo "Build Tags : ${SND_TAGS}"
 
 echo "Clearing old data..."
-rm -r build || true
-mkdir -p build/release build/release/frontend/dist build/release/data
+rm -r ${TARGET_DIR}/frontend || true
+rm -r ${TARGET_DIR}/data || true
+rm ${TARGET_DIR}/* || true
+mkdir -p ${TARGET_DIR}/frontend/dist ${TARGET_DIR}/data
 
 echo "Building App..."
 case "${GOOS}" in
@@ -28,16 +32,16 @@ LD_FLAGS="-X github.com/BigJk/snd.GitCommitHash=${GIT_COMMIT} -X github.com/BigJ
 cd cmd/app
 go build -ldflags "${LD_FLAGS}" -o app -tags "${SND_TAGS}"
 cd ../..
-mv cmd/app/app "build/release/Sales & Dungeons${EXT}"
+mv cmd/app/app "${TARGET_DIR}/${APP_NAME}${EXT}"
 
 echo "Copying frontend..."
-cp -r frontend/dist build/release/frontend
+cp -r frontend/dist ${TARGET_DIR}/frontend
 
 echo "Copying resources..."
-cp data/icon.png build/release/data/icon.png
-cp data/icon.icns build/release/data/icon.icns
+cp data/icon.png ${TARGET_DIR}/data/icon.png
+cp data/icon.icns ${TARGET_DIR}/data/icon.icns
 
 echo "Building version.txt..."
-echo "Commit: ${GIT_COMMIT}" > build/release/version.txt
-echo "Branch: ${GIT_BRANCH}" >> build/release/version.txt
-echo "Build Time: ${BUILD_TIME}" >> build/release/version.txt
+echo "Commit: ${GIT_COMMIT}" > ${TARGET_DIR}/version.txt
+echo "Branch: ${GIT_BRANCH}" >> ${TARGET_DIR}/version.txt
+echo "Build Time: ${BUILD_TIME}" >> ${TARGET_DIR}/version.txt

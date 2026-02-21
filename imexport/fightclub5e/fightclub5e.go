644796c73111d0c9da7e8794178c79883736bc15
Daniel Schmidt <info@daniel-schmidt.me>
2026-02-21 08:57:48 +0100

    fix: fightclub5e import

--- imexport/fightclub5e/fightclub5e.go
+++ imexport/fightclub5e/fightclub5e.go
@@ -1,12 +1,12 @@
 package fightclub5e
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/BigJk/snd"
 	"github.com/antchfx/xmlquery"
-	"github.com/sbabiv/xml2map"
 )
 
 func ImportCompedium(filePath string, name string, author string, slug string, description string) ([]snd.DataSource, [][]snd.Entry, error) {
@@ -30,17 +30,26 @@ func ImportCompedium(filePath string, name string, author string, slug string, d
 		var entries []snd.Entry
 
 		xmlquery.FindEach(n, "//"+types[i], func(j int, node *xmlquery.Node) {
-			data, err := xml2map.NewDecoder(strings.NewReader(node.OutputXML(true))).Decode()
-			if err == nil {
-				name := data[types[i]].(map[string]interface{})["name"].(string)
-				entryData := data[types[i]].(map[string]interface{})
-
-				entries = append(entries, snd.Entry{
-					ID:   name,
-					Name: name,
-					Data: entryData,
-				})
+			entryData, ok := xmlNodeToMap(node).(map[string]interface{})
+			if !ok {
+				return
 			}
+
+			entryName := extractEntryName(entryData["name"])
+			if entryName == "" {
+				if nameNode := xmlquery.FindOne(node, "./name"); nameNode != nil {
+					entryName = strings.TrimSpace(nameNode.InnerText())
+				}
+			}
+			if entryName == "" {
+				entryName = fmt.Sprintf("%s-%d", types[i], j+1)
+			}
+
+			entries = append(entries, snd.Entry{
+				ID:   entryName,
+				Name: entryName,
+				Data: entryData,
+			})
 		})
 
 		if len(entries) == 0 {
@@ -60,4 +69,70 @@ func ImportCompedium(filePath string, name string, author string, slug string, d
 	}
 
 	return sources, allEntries, nil
+}
+
+func xmlNodeToMap(node *xmlquery.Node) interface{} {
+	var childElements []*xmlquery.Node
+	var textBuilder strings.Builder
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		switch child.Type {
+		case xmlquery.ElementNode:
+			childElements = append(childElements, child)
+		case xmlquery.TextNode, xmlquery.CharDataNode:
+			textBuilder.WriteString(child.Data)
+		}
+	}
+
+	text := strings.TrimSpace(textBuilder.String())
+	if len(childElements) == 0 && len(node.Attr) == 0 {
+		return text
+	}
+
+	data := map[string]interface{}{}
+	for _, attr := range node.Attr {
+		key := "@" + attr.Name.Local
+		if attr.Name.Space != "" {
+			key = "@" + attr.Name.Space + ":" + attr.Name.Local
+		}
+		data[key] = attr.Value
+	}
+	if text != "" {
+		data["#text"] = text
+	}
+
+	for _, child := range childElements {
+		val := xmlNodeToMap(child)
+		if existing, ok := data[child.Data]; ok {
+			switch v := existing.(type) {
+			case []interface{}:
+				data[child.Data] = append(v, val)
+			default:
+				data[child.Data] = []interface{}{v, val}
+			}
+		} else {
+			data[child.Data] = val
+		}
+	}
+
+	return data
+}
+
+func extractEntryName(value interface{}) string {
+	switch v := value.(type) {
+	case string:
+		return strings.TrimSpace(v)
+	case map[string]interface{}:
+		if t, ok := v["#text"].(string); ok {
+			return strings.TrimSpace(t)
+		}
+	case []interface{}:
+		for _, item := range v {
+			if name := extractEntryName(item); name != "" {
+				return name
+			}
+		}
+	}
+
+	return ""
 }
